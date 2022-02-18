3845b998c387bb88eb1b144ca43c543dad656652
Tamas Czagany <tamas.czagany@gmail.com>
2022-02-18 11:39:14 +0100

    navigation chart transitions 3

--- media/animations/nav-anim-01xx-filter.js
+++ media/animations/nav-anim-01xx-filter.js
@@ -1,12 +1,15 @@
 function nav_anim_01xx_filter(chart, dirLevel) {
 
+    let crDir = 'dir' + (dirLevel - 1);
+    let nextDir = 'dir' + dirLevel;
+
     return chart.animate({
         config: {
             channels: {
-                y: { set: ['$count'], range: {min: '0%', max: '100%' } },
-                x: { set: ['dir' + dirLevel] },
-            color: { set: null },
-            label: { set: ['$count'] },
+                y: { set: ['$count', nextDir], range: {min: '0%', max: '100%' } },
+                x: { set: [crDir] },
+                color: { set: null },
+                label: { set: null }
             },
             title: null,
             legend: null
@@ -20,6 +23,42 @@ function nav_anim_01xx_filter(chart, dirLevel) {
             }
         }},
         { duration: 1 }
-    );
-}
+    )
 
+    .then(chart => chart.animate({
+        config: {
+            channels: {
+                y: { set: ['$count'], range: {min: '0%', max: '100%' } },
+                x: { set: [crDir, nextDir] }
+            }
+        },
+        style: {
+            plot: {
+                paddingLeft: '13em',                
+                marker: { label: { position: 'center', fontSize: '1.3em'} },
+                    xAxis: { label: { angle: 0, fontSize:  '1em' },
+                             title: { paddingTop: '2.5em' } }
+            }
+        }},
+        { duration: 1 }
+    ))
+
+    .then(chart => chart.animate({
+        config: {
+            channels: {
+                y: { set: ['$count'], range: {min: '0%', max: '100%' } },
+                x: { set: [nextDir] },
+                label: { set: ['$count'] }
+            }
+        },
+        style: {
+            plot: {
+                paddingLeft: '13em',                
+                marker: { label: { position: 'center', fontSize: '1.3em'} },
+                    xAxis: { label: { angle: 0, fontSize:  '1em' },
+                             title: { paddingTop: '2.5em' } }
+            }
+        }},
+        { duration: 1 }
+    ));
+}
